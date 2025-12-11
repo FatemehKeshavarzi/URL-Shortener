f2cb8c38e5b3cd7c18f2007c573aa085e52c4487
raha1382 <143132177+raha1382@users.noreply.github.com>
2025-12-11 23:41:06 +0330

    feat: create based-62 function.

---
+++ app/utils/validator.py
@@ -0,0 +1,12 @@
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def encode_base62(num: int) -> str:
+    if num == 0:
+        return ALPHABET[0]
+    arr = []
+    base = len(ALPHABET)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(ALPHABET[rem])
+    arr.reverse()
+    return ''.join(arr)
