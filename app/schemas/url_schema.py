ca5d97ddfb43b57ca77035bf73e4afdbbb854418
raha1382 <143132177+raha1382@users.noreply.github.com>
2025-12-12 16:22:13 +0330

    refactor: correct ulr format.

--- app/schemas/url_schema.py
+++ app/schemas/url_schema.py
@@ -9,9 +9,9 @@ class URLCreate(BaseModel):
     def validate_url(cls, v):
         url = v.strip()
 
-        allowed_tlds = [".com", ".ir", ".net"]
-        if not any(url.endswith(tld) for tld in allowed_tlds):
-            raise ValueError(f"URL must end with one of {allowed_tlds}")
+        # allowed_tlds = [".com", ".ir", ".net"]
+        # if not any(url.endswith(tld) for tld in allowed_tlds):
+        #     raise ValueError(f"URL must end with one of {allowed_tlds}")
         
         try:
             HttpUrl.validate(url)
