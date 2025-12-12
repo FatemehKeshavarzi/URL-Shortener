34d61d0912b899c264f580524232f55a20f3abad
FatemehKeshavarzi <fatemeh.kesha98@gmail.com>
2025-12-12 18:36:31 +0330

    add list and delete url routers

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
-from app.api import url_router
-from app.repositories.url_repository import URLRepository
-from app.db.session import SessionLocal
+from app.api.routers import url_router
+
 
 app = FastAPI(
     title="URL Shortener",
@@ -16,9 +15,3 @@ app.include_router(url_router.router)
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host='127.0.0.1', port=8000)
-
-# @app.on_event("startup")
-# async def startup_event():
-#     db = SessionLocal()
-#     url_repo = URLRepository(db)
-#     print("Application startup complete")
