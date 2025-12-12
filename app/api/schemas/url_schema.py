0075eea9c49f7fede78b98eb8380682b080588ad
FatemehKeshavarzi <fatemeh.kesha98@gmail.com>
2025-12-12 18:44:49 +0330

    add URLListSchema

--- app/api/schemas/url_schema.py
+++ app/api/schemas/url_schema.py
@@ -20,3 +20,9 @@ class URLResponse(BaseModel):
         from_attributes = True
 
 
+class URLListSchema(BaseModel):
+    id: int
+    original_url: str
+    short_code: str
+    created_at: datetime
+    expires_at: datetime | None = None
